3e8686ebf3076c97b189c22f7e8414613da743a8
ben tancock <ben.tancock@gmail.com>
2025-04-29 16:53:11 -0400

    comments

--- src/scripts/top-workplaces.ts
+++ src/scripts/top-workplaces.ts
@@ -10,14 +10,8 @@ import { PrismaClient } from '@prisma/client';
 // from shifts we can count workplaceId, make a list of workplaceId, map to name with /workplaces
 // but this is in /scripts not modules... so maybe something different?
 
-
-// just a test function, seeing if I can get this called somewhere else
 // I'm pretty sure a call like this would be called in workplaces (e.g. workplaces/top-workplaces)
 // I don't think they want a new api call added though, given this isn't a node module or anything
-
-
-// OR we could make our own calls which would just fetch us all the data and then sort it here
-// just to show that I can I'm going to make my own calls to get everything and then sort it + display it
 
 const prisma = new PrismaClient();
 
